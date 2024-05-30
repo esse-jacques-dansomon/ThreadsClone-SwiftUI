343a5ed7b806fe81721dde4d16615a37cc98c0a3
esse.jacques.co <essedansomon@gmail.com>
2024-05-30 21:55:37 +0000

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Esse Jacques  on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
