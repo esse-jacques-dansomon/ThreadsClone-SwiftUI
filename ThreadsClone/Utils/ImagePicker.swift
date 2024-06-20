1e7878853d86b763ff4efc6408c7136320b15b4a
esse.jacques.co <essedansomon@gmail.com>
2024-06-20 01:39:27 +0000

    feat: add images and video to thread

---
+++ ThreadsClone/Utils/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  ThreadsClone
+//
+//  Created by Esse Jacques  on 19/06/2024.
+//
+
+import Foundation
---
+++ ThreadsClone/Utils/VideoPicker.swift
@@ -0,0 +1,8 @@
+//
+//  VideoPicker.swift
+//  ThreadsClone
+//
+//  Created by Esse Jacques  on 19/06/2024.
+//
+
+import Foundation
